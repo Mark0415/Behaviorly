2b2a8f9185768dd954c7fa78201d82fd0727f692
Mark Valdez <mbqvaldez@gmail.com>
2021-05-20 11:32:24 -0700

    update goal check

--- components/GoalCheck.js
+++ components/GoalCheck.js
@@ -1,12 +1,25 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-filename-extension */
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, TextInput, View } from 'react-native';
 
-const GoalCheck = ({ client }) => (
-  <View>
-    <Text>{`How did ${client} do on her goals?`}</Text>
-  </View>
-);
+const GoalCheck = ({ client, clientGoals }) => {
+  const [goalMet, setGoalMet] = useState([]);
+  const checkGoals = (name) => {
+    for (let i = 0; i < clientGoals.lenght; i += 1) {
+      console.log('these are goals', clientGoals[i]);
+      clientGoals[i].name === name ? setGoalMet(client[i].goals) : null;
+    }
+    goalMet.map((met) => <Text>{met}</Text>);
+  };
+
+  return (
+    <View>
+      <TextInput
+        placeholder="Brief summarized title"
+      />
+    </View>
+  );
+};
 
 export default GoalCheck;
